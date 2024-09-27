7c9f5a3e2c2b67bdfa001557e69e3a994ed65427
Ahbabuzzaman <160356472+ahbab-zaman@users.noreply.github.com>
2024-09-27 18:43:03 +0600

    aoifhasoi

---
+++ practice.js
@@ -0,0 +1,44 @@
+// Problem 1 --> given an array of number and you have to filter out all the odd numbers
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const oddNumbers = numbers.filter((number) => number % 2 === 1);
+// console.log(oddNumbers)
+
+// Problem - 2 --> Given an array of numbers and you have to access every element and return a greetings with the names.
+
+// const names = ['ahbab', 'talha', 'shazeed', 'emad', 'abdullah'];
+// const greet = names.forEach((name) => console.log(`Hello : ${name}`));
+
+// Problem - 3 --> filter and reduce combine related problem
+
+// const scores = [70, 55, 90, 65, 80, 95, 45];
+// const newScores = scores.filter((score) =>  score >= 80)
+// console.log(newScores)
+// const sum = newScores.reduce((prev, curr) => prev + curr, 0)
+// console.log(sum)
+// const average = sum / newScores.length;
+// console.log(average)
+
+// Problem - 4
+
+// map and filter related problem
+
+// const numbers = [12, 3, 5, 6, 7, 10];
+// const tripledNumbers = numbers.map((number) => number * 3);
+// console.log(tripledNumbers)
+// const getNumberAbove10 = tripledNumbers.filter((num) => num > 20);
+// console.log(getNumberAbove10)
+
+// Problem - 5
+
+// forEach related problem
+
+// const products = [
+//     { name: 'Laptop', price: 800 },
+//     { name: 'Phone', price: 500 },
+//     { name: 'Tablet', price: 300 }
+//   ];
+// const getNamePrice = products.forEach((product) =>{
+//     console.log(`${product.name} : ${product.price}$`)
+// })
+// console.log(getNamePrice)
---
+++ script.js
@@ -0,0 +1,49 @@
+// Rest operator
+
+// const number = [1, 2, 3, 4, 5];
+// const [first, ...second] = number;
+// console.log(number);
+// console.log(first)
+// console.log(second)
+
+// Dot notation and bracket notation 
+
+// Dot Notation : It is used for accessing to a item through . symbol
+// Bracket Notation : It is used for accessing to an item through () symbol. Using bracket notation is more correct than dot notation.
+
+// map method are used for operate all the elements over an array and it return a new array.
+
+// const number = [1, 2, 3, 4, 5];
+// const doubledNumber = number.map((num)=>{
+//     return num * 2;
+// });
+// console.log(doubledNumber);
+
+// Filter method
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const evenNumbers = numbers.filter(function(number){
+//     return number % 2 === 0;
+// })
+// console.log(evenNumbers)
+
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const evenNumbers = numbers.filter((number) => number % 2 === 0)
+// console.log(evenNumbers)
+
+// forEach method
+
+// forEach method basically iterates over every element of an array an does'nt return a new array.
+
+// const fruits = ['apple', 'banana', 'litchi', 'mango'];
+// fruits.forEach((index, fruit) => {
+//    console.log(`${index} : ${fruit}`)
+// })
+
+// reduce method
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const sum = numbers.reduce((prev, curr) => prev + curr, 0)
+// console.log(sum)
+
